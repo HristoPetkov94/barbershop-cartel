7a549c472d35d79711dd3e8098f37b55d7034312
pmarinov <marinov@lexoro.ai>
2020-08-22 12:45:05 +0300

    improved animation and added the other tabs.

--- frontend/app/src/app/home/home.component.ts
+++ frontend/app/src/app/home/home.component.ts
@@ -4,6 +4,7 @@ import {animate, style, transition, trigger} from '@angular/animations';
 
 // TODO: Refactor everything here methods, split into components if you have to, refactor css to make it more readable, delete unused stuff.
 
+// @ts-ignore
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
@@ -13,7 +14,7 @@ import {animate, style, transition, trigger} from '@angular/animations';
       trigger('slideInOut', [
         transition(':enter', [
           style({transform: 'translateX(+100%)'}),
-          animate('200ms ease-in', style({transform: 'translateX(0%)', transition: '2s'}))
+          animate('300ms ease-in', style({transform: 'translateX(0%)', transition: '2s'}))
         ]),
       ]),
     ]
