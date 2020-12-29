ccbaee300c7125f836117709368e6f90c73ee430
pmarinov <marinov@lexoro.ai>
2020-12-29 22:07:58 +0200

    created general config panel

---
+++ frontend/app/src/app/admin-configuration-panel/general-configuration/general-configuration.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-general-configuration',
+  templateUrl: './general-configuration.component.html',
+  styleUrls: ['./general-configuration.component.css', '../shared-styles/shared.css']
+})
+export class GeneralConfigurationComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
