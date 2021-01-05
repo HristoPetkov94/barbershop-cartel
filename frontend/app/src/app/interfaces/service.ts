75edcc2504d184208e19f64119c56f03bb6e98a2
petkov <petkov@lexoro.ai>
2021-01-05 11:55:52 +0200

    removing constructors which are setting default values of the objects

--- frontend/app/src/app/interfaces/service.ts
+++ frontend/app/src/app/interfaces/service.ts
@@ -5,14 +5,5 @@ export class Service {
   duration: number;
   description: string;
   picture: any;
-  deleted = false;
-
-  constructor() {
-    this.serviceType = '';
-    this.price = 0;
-    this.duration = 0;
-    this.description = '';
-    this.picture = '';
-    this.deleted = false;
-  }
+  deleted: boolean;
 }
--- frontend/app/src/app/models/barber.ts
+++ frontend/app/src/app/models/barber.ts
@@ -1,22 +1,11 @@
 import {Service} from '../interfaces/service';
 
 export class Barber {
-
   id: number;
-  firstName?: string;
+  firstName: string;
   lastName: string;
-  description?: string;
-  picture?: string;
-  services?: Service[];
-  deleted?: boolean;
-
-  constructor() {
-    this.id = undefined;
-    this.firstName = '';
-    this.lastName = '';
-    this.description = '';
-    this.picture = '';
-    this.services = [];
-    this.deleted = false;
-  }
+  description: string;
+  picture: string;
+  services: Service[];
+  deleted: boolean;
 }
