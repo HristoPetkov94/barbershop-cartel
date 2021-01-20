b3e79313cc3ea29f86498f1752ba2593e50c8ed9
pmarinov <marinov@lexoro.ai>
2021-01-20 15:43:50 +0200

    added checks for security if token is expired or invalid to clear session storage and redirect

--- frontend/app/src/app/app-routing.module.ts
+++ frontend/app/src/app/app-routing.module.ts
@@ -2,22 +2,30 @@ import {NgModule} from '@angular/core';
 import {Routes, RouterModule} from '@angular/router';
 import {HomeComponent} from './home/home.component';
 import {LoginComponent} from './login/login.component';
-import {DataConfigurationPanelComponent} from './configuration-panel/data-configuration-panel.component';
 import {ConfigurationComponent} from './admin-configuration-panel/configuration.component';
 import {AuthGuardService} from './authentication/auth-guard.service';
 import {BarberBookNowPanelComponent} from './barber-book-now-panel/barber-book-now-panel.component';
 import {BarberDashboardComponent} from './barber-dashboard/barber-dashboard.component';
 import {RegisterComponent} from './register/register.component';
+import {BarberTeamPanelComponent} from './barber-team-panel/barber-team-panel.component';
+import {BarberServicesPanelComponent} from './barber-services-panel/barber-services-panel.component';
+import {ContactsStackComponent} from './stacks/contacts-stack/contacts-stack.component';
 
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
+  // { path: '**', redirectTo: '/'},
+
+  { path: 'team', component: BarberTeamPanelComponent, data: {animation: 'HomePage'} },
+  { path: 'services', component: BarberServicesPanelComponent, data: {animation: 'HomePage'} },
+  { path: 'contacts', component: ContactsStackComponent, data: {animation: 'HomePage'} },
+
   { path: 'book-now', component: BarberBookNowPanelComponent },
   { path: 'login', component: LoginComponent},
   { path: 'register', component: RegisterComponent},
   // TODO: rename and change path to more appropriate one.
   // { path: 'configuration', component: DataConfigurationPanelComponent, canActivate: [AuthGuardService]},
-  { path: 'configuration', component: ConfigurationComponent},
+  { path: 'configuration', component: ConfigurationComponent, canActivate: [AuthGuardService]},
   { path: 'dashboard', component: BarberDashboardComponent, canActivate: [AuthGuardService]}
 ];
 
--- frontend/app/src/app/interceptors/basic-auth-http.interceptor.service.ts
+++ frontend/app/src/app/interceptors/basic-auth-http.interceptor.service.ts
@@ -1,12 +1,17 @@
 import {Injectable} from '@angular/core';
-import {HttpInterceptor, HttpRequest, HttpHandler} from '@angular/common/http';
+import {HttpInterceptor, HttpRequest, HttpHandler, HttpResponse, HttpErrorResponse} from '@angular/common/http';
+import {tap} from 'rxjs/operators';
+import {Router} from '@angular/router';
 
 @Injectable({
   providedIn: 'root'
 })
 export class BasicAuthHttpInterceptorService implements HttpInterceptor {
 
-  constructor() {
+  private static readonly UNAUTHORIZED_STATUS = 401;
+  private static readonly UNAUTHORIZED_TEXT = 'Unauthorized';
+
+  constructor(private router: Router) {
   }
 
   intercept(req: HttpRequest<any>, next: HttpHandler) {
@@ -20,6 +25,23 @@ export class BasicAuthHttpInterceptorService implements HttpInterceptor {
       });
     }
 
-    return next.handle(req);
+    return next.handle(req)
+      .pipe(
+        tap(() => {
+          },
+          (err: any) => {
+            if (err instanceof HttpErrorResponse) {
+              // event equals a response
+              // 401 means unauthorized
+              if (err.status === BasicAuthHttpInterceptorService.UNAUTHORIZED_STATUS
+                && err.statusText === BasicAuthHttpInterceptorService.UNAUTHORIZED_TEXT) {
+
+                sessionStorage.clear();
+                this.router.navigate(['login']);
+              }
+            }
+          }
+        )
+      );
   }
 }
