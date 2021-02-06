a29c284da954041a91fa9c0afd1f0f05d80be871
stoychev <stoychev@lexoro.ai>
2021-02-06 23:26:05 +0200

    Temporary page added so the shop does not appear like its broken.

--- frontend/app/src/app/views/store-view/store.component.ts
+++ frontend/app/src/app/views/store-view/store.component.ts
@@ -1,11 +1,14 @@
 import {Component, OnInit, ViewChild} from '@angular/core';
 import {StoreService} from '../../services/store.service';
 import {NotificationComponent} from '../../notification/notification.component';
+import {fade} from "../animations/fade";
 
 @Component({
   selector: 'app-store-view',
   templateUrl: './store.component.html',
-  styleUrls: ['./store.component.css']
+  styleUrls: ['./store.component.css'], animations: [
+    fade
+  ]
 })
 export class StoreComponent implements OnInit {
 
