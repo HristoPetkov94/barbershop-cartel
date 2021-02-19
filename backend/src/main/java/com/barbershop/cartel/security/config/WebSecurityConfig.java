f29d50a3522117db5c041cd369d08aea1ec37141
stoychev <stoychev@lexoro.ai>
2021-02-19 16:46:31 +0200

    Spring security changes.

--- backend/src/main/java/com/barbershop/cartel/security/config/WebSecurityConfig.java
+++ backend/src/main/java/com/barbershop/cartel/security/config/WebSecurityConfig.java
@@ -55,25 +55,11 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity httpSecurity) throws Exception {
 
-        String[] getMappings = new String[] {
-                "/user","/general-configuration/**", "/images/**"
-        };
-
-        String[] postMappings = new String[] {
-                "/barbers", "/services"
-        };
-
-        // We don't need CSRF for this example
         httpSecurity.csrf().disable()
-                // dont authenticate this particular request
                 .authorizeRequests()
-                .antMatchers(HttpMethod.GET, getMappings).permitAll()
-                .antMatchers(HttpMethod.POST, postMappings).permitAll()
-                // all other requests need to be authenticated
-                        .anyRequest().authenticated().and()
-                // make sure we use stateless session; session won't be used to
-                // store user's state.
-                        .exceptionHandling().authenticationEntryPoint(jwtAuthenticationEntryPoint).and().sessionManagement()
+                .anyRequest().authenticated().and()
+                // make sure we use stateless session; session won't be used to store user's state.
+                .exceptionHandling().authenticationEntryPoint(jwtAuthenticationEntryPoint).and().sessionManagement()
                 .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
 
         // Add a filter to validate the tokens with every request
