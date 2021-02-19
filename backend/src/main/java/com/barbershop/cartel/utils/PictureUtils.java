3a31fede9f345130118b912bb7ec327266156b8a
stoychev <stoychev@lexoro.ai>
2021-02-19 15:51:08 +0200

    Read from resources fixed on server fixed.

--- backend/src/main/java/com/barbershop/cartel/utils/PictureUtils.java
+++ backend/src/main/java/com/barbershop/cartel/utils/PictureUtils.java
@@ -2,19 +2,20 @@ package com.barbershop.cartel.utils;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.core.io.ClassPathResource;
+import org.springframework.util.ResourceUtils;
 
 import java.io.File;
 
 @Slf4j
 public class PictureUtils {
-    private final static String DEFAULT_PICTURE_PATH = "images/default-profile-picture.png";
+    private final static String DEFAULT_PICTURE_PATH = "classpath:images/default-profile-picture.png";
 
     public static String getDefaultPicture() {
 
         String picture = "";
 
         try {
-            File f = new ClassPathResource(DEFAULT_PICTURE_PATH).getFile();
+            File f = ResourceUtils.getFile(DEFAULT_PICTURE_PATH);
             picture = Base64Util.encodeFileToBase64Binary(f);
         } catch (Exception e) {
             log.error("Default picture is missing");
