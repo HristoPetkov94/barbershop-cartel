af16a1202a26d52b7e9792a247b19964172a6cff
stoychev <stoychev@lexoro.ai>
2021-03-02 01:09:58 +0200

    Tooltips

--- frontend/app/src/app/views/components/compact-navbar/compact-navbar.component.ts
+++ frontend/app/src/app/views/components/compact-navbar/compact-navbar.component.ts
@@ -1,31 +1,11 @@
-import {Component, OnInit} from '@angular/core';
-import {getCookie, setCookie} from '../../../utils/cookie.utils';
-import {TranslateService} from '@ngx-translate/core';
+import {Component} from '@angular/core';
+import {NavigationComponent} from '../navigation/navigation.component';
 
 @Component({
   selector: 'app-compact-navbar',
   templateUrl: './compact-navbar.component.html',
   styleUrls: ['./compact-navbar.component.css']
 })
-export class CompactNavbarComponent implements OnInit {
+export class CompactNavbarComponent extends NavigationComponent {
   public hideBurger = false;
-
-  constructor(public translate: TranslateService) {
-  }
-
-  ngOnInit() {
-    const cookie = getCookie('lang');
-    const preferredLang = cookie === undefined ? 'bg' : cookie;
-
-    this.translate.use(preferredLang);
-  }
-
-  changeLang() {
-    const currentLang = this.translate.currentLang;
-    const lang = currentLang === 'en' ? 'bg' : 'en';
-
-    setCookie('lang', lang);
-
-    this.translate.use(lang);
-  }
 }
