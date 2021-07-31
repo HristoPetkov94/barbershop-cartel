fa3a4d10738b1d0ec1485ad71fd8e97e7a0f1589
pmarinov <marinov@lexoro.ai>
2021-07-31 20:46:54 +0300

    added api info to swagger

--- backend/src/main/java/com/barbershop/cartel/security/config/SwaggerConfig.java
+++ backend/src/main/java/com/barbershop/cartel/security/config/SwaggerConfig.java
@@ -4,9 +4,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.service.ApiKey;
-import springfox.documentation.service.AuthorizationScope;
-import springfox.documentation.service.SecurityReference;
+import springfox.documentation.service.*;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spi.service.contexts.SecurityContext;
 import springfox.documentation.spring.web.plugins.Docket;
@@ -22,6 +20,7 @@ public class SwaggerConfig {
     @Bean
     public Docket api() {
         return new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(apiInfo())
                 .securityContexts(Collections.singletonList(securityContext()))
                 .securitySchemes(Collections.singletonList(apiKey()))
                 .select()
@@ -29,6 +28,19 @@ public class SwaggerConfig {
                 .paths(PathSelectors.any())
                 .build();
     }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfo(
+                "CARTEL REST API",
+                "Barbershop CARTEL's REST API",
+                "1.0",
+                "#",
+                new Contact("Dev Friday", "#", "#"),
+                "#",
+                "#",
+                Collections.emptyList());
+    }
+
 
     private ApiKey apiKey() {
         return new ApiKey("JWT", "Authorization", "header");
